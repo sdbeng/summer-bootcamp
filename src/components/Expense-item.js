b572cfe6d316be5d89bbdad47ce377ace16d6aae
Sergio D. Barrera <sdbeng@gmail.com>
2021-06-01 23:27:53 -0700

    render dummy data from local object

--- src/components/Expense-item.js
+++ src/components/Expense-item.js
@@ -1,13 +1,25 @@
 import './expense-item.css'
 
+const DUMMY_DATA = [
+    {
+        title:'Expense Item 1',
+        date: new Date(2021,6,2).toISOString(),
+        description:'Item 1 description',
+        price:249.99
+    },
+]
+
+
 function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <h2>Expense Item TITLE</h2>
+            <h2>{DUMMY_DATA[0].title}</h2>
 
-            <div><p>DATE</p></div>
-            <div><p>DESC</p></div>
-            <div><h3>PRICE</h3></div>
+            <div><p>{DUMMY_DATA[0].date}</p></div>
+            <div className="expense-item__desc">
+                <p>{DUMMY_DATA[0].description}</p>
+            </div>
+            <div><h3>${DUMMY_DATA[0].price}</h3></div>
         </div>
     )
 }
