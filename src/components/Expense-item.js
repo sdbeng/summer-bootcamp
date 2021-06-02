078459b68893859064b4f99ab506a26ec951f7f6
Sergio D. Barrera <sdbeng@gmail.com>
2021-06-01 22:54:19 -0700

    init component

---
+++ src/components/Expense-item.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ExpenseItem(props) {
+    return (
+        <div>
+            <h2>Expense Item TITLE</h2>
+            <div><p>DESC</p></div>
+            <div><h3>PRICE</h3></div>
+        </div>
+    )
+}
+
+export default ExpenseItem
